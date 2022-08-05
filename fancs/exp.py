0eb6ddef39ddb183ebbd579a49e94ebdea8e7146
Dmitriy <zeifd@mail.ru>
2022-08-05 21:53:34 +0700

    Добавил отдельную папку data для файлов с данными

--- fancs/exp.py
+++ fancs/exp.py
@@ -1,9 +1,9 @@
 import controller 
 
 def btn_exp_click():
-    file = controller.filedialog.asksaveasfilename(defaultextension=f'{controller.combo_imp.get()}', filetypes = ((f'{controller.combo_imp.get()}',f'*.{controller.combo_imp.get()}'),("all files","*.*")))
+    file = controller.filedialog.asksaveasfilename(defaultextension=f'{controller.combo_imp.get()}', initialdir="data\exp", filetypes = ((f'{controller.combo_imp.get()}',f'*.{controller.combo_imp.get()}'),("all files","*.*")))
     spisok = []
-    with open('spr.csv', 'r', encoding='utf-8') as file2:
+    with open('data\spr.csv', 'r', encoding='utf-8') as file2:
         spisok = file2.read()
 
     with open(file, 'a', encoding='utf-8') as file:
--- fancs/imp.py
+++ fancs/imp.py
@@ -1,10 +1,10 @@
 import controller 
 
 def btn_imp_click():
-    file = controller.filedialog.askopenfilename(filetypes = ((f'{controller.combo_imp.get()}',f'*.{controller.combo_imp.get()}'),("all files","*.*")))
+    file = controller.filedialog.askopenfilename(initialdir="data\imp", filetypes = ((f'{controller.combo_imp.get()}',f'*.{controller.combo_imp.get()}'),("all files","*.*")))
     spisok = []
     with open(file, 'r', encoding='utf-8') as file:
         spisok = file.read()
 
-    with open('spr.csv', 'a', encoding='utf-8') as file2:
+    with open('data\spr.csv', 'a', encoding='utf-8') as file2:
         file2.write(spisok)
